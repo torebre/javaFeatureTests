a5c8860cf09d9ef3d2fa01d7168dc3d287ff13ab
Tore Brede <tore.brede@gmail.com>
2023-03-15 22:28:07 +0100

    Looking at vector API

---
+++ src/main/java/com/kjipo/VectorTest.java
@@ -0,0 +1,46 @@
+package com.kjipo;
+
+import jdk.incubator.vector.IntVector;
+import jdk.incubator.vector.VectorOperators;
+
+import java.util.stream.IntStream;
+
+/**
+ * Tested with Java 20
+ */
+public class VectorTest {
+
+
+    private static void vectorTest() {
+        var species = IntVector.SPECIES_PREFERRED;
+        var intArray = IntStream.range(0, 200).toArray();
+        var intArray2 = IntStream.range(0, 200).toArray();
+
+        var finalResult = new int[200];
+        var sumVector = IntVector.zero(species);
+
+        for (int i = 0; i < intArray.length; i += species.length()) {
+            // Not all CPUs supports masks
+            var mask = species.indexInRange(i, intArray.length);
+            var vector = IntVector.fromArray(species, intArray, i, mask);
+            var vector2 = IntVector.fromArray(species, intArray2, i, mask);
+
+            var result = vector.add(vector2, mask);
+            result.intoArray(finalResult, i, mask);
+
+            sumVector = sumVector.add(result);
+        }
+
+        IntStream.of(finalResult).boxed().map(value -> value + ", ").forEach(System.out::print);
+        System.out.println();
+
+        var sum = sumVector.reduceLanes(VectorOperators.ADD);
+        System.out.println("Sum: " + sum);
+    }
+
+    public static void main(String[] args) {
+        vectorTest();
+
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module feature.test.module {
     requires jdk.incubator.concurrent;
+    requires jdk.incubator.vector;
 }
