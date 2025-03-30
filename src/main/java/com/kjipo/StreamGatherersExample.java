ffc3535c5b108bce766131070ebe54f0bab433b2
Tore Brede <tore.brede@gmail.com>
2025-03-30 16:46:01 +0200

    Following an example showing use of stream gatherers

--- src/main/java/com/kjipo/StreamGatherersExample.java
+++ src/main/java/com/kjipo/StreamGatherersExample.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Gatherer;
 import java.util.stream.LongStream;
 
+
 public class StreamGatherersExample {
     private static final SplittableRandom random = new SplittableRandom();
 
@@ -18,34 +19,50 @@ public class StreamGatherersExample {
                 .toList();
 
 
-        // TODO
+        Map<Integer, List<DataRecord>> valueDataRecordMap = dataRecords.stream()
+                .gather(createGatherer(DataRecord::value,
+                        10,
+                        Comparator.comparing(DataRecord::timeStamp)))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
-//        dataRecords.stream()
-//                .gather(createGatherer(DataRecord::value,
-//                        10,
-//                        Comparator.comparing(DataRecord::timeStamp)))
-//                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
+        valueDataRecordMap.forEach((key, dataRecordsForValue) -> {
+            System.out.println("Key: " + key);
+            for (DataRecord dataRecord : dataRecordsForValue) {
+                System.out.println(dataRecord);
+            }
+        });
 
     }
 
 
-//    private static <K> Gatherer<DataRecord, Map<K, List<DataRecord>>, List<DataRecord>> createGatherer(Function<? super DataRecord, ? extends K> keyExtractor,
-//                                                                                                       int limit,
-//                                                                                                       Comparator<? super DataRecord> comparator
-//    ) {
-//
-//        return Gatherer.of(
-//                HashMap::new,
-//                (map, dataRecord, downstream) -> {
-//                    K key = keyExtractor.apply(dataRecord);
-//
-//                }
-//
-//        );
-//
-//
-//    }
+    private static <K> Gatherer<DataRecord, Map<K, List<DataRecord>>, Map.Entry<K, List<DataRecord>>> createGatherer(Function<? super DataRecord, ? extends K> keyExtractor,
+                                                                                                                     int limit,
+                                                                                                                     Comparator<? super DataRecord> comparator
+    ) {
+
+        return Gatherer.ofSequential(
+                HashMap::new,
+
+                (map, dataRecord, downstream) -> {
+                    K key = keyExtractor.apply(dataRecord);
+                    map.computeIfAbsent(key, _ -> new ArrayList<>(limit)).add(dataRecord);
+                    return true;
+
+                },
+
+                (map, downstream) -> {
+                    map.forEach((key, dataRecords) -> {
+                        List<DataRecord> sortedAndLimited = dataRecords.stream()
+                                .sorted(comparator.reversed())
+                                .limit(limit)
+                                .toList();
+
+                        downstream.push(Map.entry(key, sortedAndLimited));
+                    });
+                }
+        );
+
+    }
 
 
     private static DataRecord generateDataRecord(long inputId) {
